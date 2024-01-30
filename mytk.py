d0f5d5b4e9c8cf95033dbe3b2846a5f47a1d0ea4
Daniel Côté <dccote@cervo.ulaval.ca>
2024-01-29 17:52:50 -0800

    Update mytk.py
    MyTk wrappe ris starting to make sense and be useful.  I finally understand grid and pack, and Frame is like a view in macOS.

--- mytk.py
+++ mytk.py
@@ -16,69 +16,66 @@ class MyTkApp:
         return cls.app
 
     def __init__(self, geometry=None):
-        self.root = Tk()
-        self.window = self.new_window(geometry=geometry)
-
-    def new_window(self, geometry=None):
         self.window = MyWindow(geometry)
 
-    def mainloop(self):
-        self.root.mainloop()
-
-class MyTkClass:
-    def __init__(self):
-        self.main_widget = None
+    @property
+    def main_window(self):
+        return self.window
 
     @property
     def root(self):
-        return MyTkApp.app.root
+        return self.main_window.root
     
-    def grid(self, column, row):
-        if self.main_widget is not None:
-            self.main_widget.grid(column=column, row=row)
+    def mainloop(self):
+        self.root.mainloop()
 
-    def pack(self):
-        if self.main_widget is not None:
-            self.main_widget.pack()
+class MyTkBaseView:
+    def __init__(self, master):
+        self.main_widget = None
+        self.parent = master
 
-class MyFrame(MyTkClass):
+    def grid(self, **kwargs):
+        if self.main_widget is not None:
+            self.main_widget.grid(kwargs)
+
+    def pack(self, **kwargs):
+        if self.main_widget is not None:
+            self.main_widget.pack(kwargs)
+
+class MyView(MyTkBaseView):
     def __init__(self, master, width, height):
-        MyTkClass.__init__(self)
-        self.frame = ttk.Frame(master, width=width, height=height, padding=10)
+        MyTkBaseView.__init__(self, master)
+        self.frame = ttk.Frame(master, width=width, height=height)
         self.main_widget = self.frame
 
-class MyWindow(MyTkClass):
+class MyWindow(MyTkBaseView):
     def __init__(self, geometry = None, title="Viewer"):
-        MyTkClass.__init__(self)
-        self.frame = ttk.Frame(self.root, padding=10)
-
-        self.title = title
-        self.create_window(geometry)
-
-    def create_window(self, geometry = None):
         if geometry is None:
             geometry = "1020x750"
+        self.title = title
+
+        self.root = Tk()
         self.root.geometry(geometry)
         self.root.title(self.title)
+        MyTkBaseView.__init__(self, self.root)
 
-class MyPopupMenu(MyTkClass):
+class MyPopupMenu(MyTkBaseView):
     def __init__(self, master, menu_items = None):
-        MyTkClass.__init__(self)
+        MyTkBaseView.__init__(self, master)
+
+        self.selected_index = None
+        self.user_callback = None
 
         self.text = StringVar(value="Select menu item")
         self.menu = Menu(master, tearoff=0)
-        self.selected_index = None
-        self.menu_items = None
-        self.main_widget = ttk.Menubutton(textvariable=self.text, text='All lenses', menu=self.menu)
-        self.main_widget.width = 30
-        self.user_callback = None
+        self.main_widget = ttk.Menubutton(master, textvariable=self.text, text='All lenses', menu=self.menu)
 
-        if menu_items is not None:
+        if  menu_items is not None:
             self.add_menu_items(menu_items)
+
 
     def add_menu_items(self, menu_items, user_callback = None):
         self.menu_items = menu_items
-
         labels = menu_items
         for i, label in enumerate(labels):
             self.menu.add_command(label=label, command=partial(self.selection_callback, i))
@@ -90,42 +87,41 @@ class MyPopupMenu(MyTkClass):
         if self.user_callback is not None:
             self.user_callback()
 
-class MyLabel(MyTkClass):
+class MyLabel(MyTkBaseView):
     def __init__(self, master, text=""):
-        MyTkClass.__init__(self)
+        MyTkBaseView.__init__(self, master)
         self.main_widget = ttk.Label(master, text=text)
 
 
-class MyMatplotlibView(MyTkClass):
+class MyMatplotlibView(MyTkBaseView):
     def __init__(self, master, figure=None):
-        MyTkClass.__init__(self)
+        MyTkBaseView.__init__(self, master)
 
         if figure is None:
-            self.figure = Figure(figsize=(10.2, 7.5), dpi=100)
+            self.figure = Figure(figsize=(10.2, 5.5), dpi=100)
         else:
             self.figure = figure
 
         self.canvas = FigureCanvasTkAgg(self.figure, master=master)
-        toolbar = NavigationToolbar2Tk(self.canvas, master, pack_toolbar=False)
+        toolbar = NavigationToolbar2Tk(self.canvas, master, pack_toolbar=True)
         toolbar.update()
 
         self.main_widget = self.canvas.get_tk_widget()
 
 if __name__ == "__main__":
     app = MyTkApp()
-    top_frame = MyFrame(app.root, width=1000,  height=100)
-    top_frame.grid(0,0)
-    # top_frame.main_widget.pack()
-    # bottom_frame = MyFrame(app.root, width=1000,  height=700)
-    # top_frame.main_widget.pack()
+    top_frame = MyView(app.root, width=500,  height=100)
+    top_frame.grid(column=0, row=0, pady=20)
+    bottom_frame = MyView(app.root, width=1000,  height=600)
+    bottom_frame.grid(column=0, row=1, pady=20)
 
     label = MyLabel(top_frame.main_widget, "Testalksdhlak")
-    label.grid(0,0)
+    label.grid(column=0,row=0)
     menu = MyPopupMenu(top_frame.main_widget, ["aasdasda","basfasdfada","csdfsdfsdfsd"])
-    menu.grid(1,0)
+    menu.grid(column=1,row=0)
 
-    view = MyMatplotlibView(app.root)
-    view.grid(0,1)
+    view = MyMatplotlibView(bottom_frame.main_widget)
+    view.pack()
     plot = view.figure.add_subplot()
     plot.plot([0,1,2,3], [4,5,6,7])
 
