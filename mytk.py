6506d6d449b9f87cf7ed18553eaeda653dc25459
Daniel Côté <dccote@cervo.ulaval.ca>
2024-02-22 11:09:34 -0500

    Update mytk.py

--- mytk.py
+++ mytk.py
@@ -1274,22 +1274,22 @@ class Slider(Base):
             self.delegate.value_updated(object=self, value_variable=self.value_variable)
 
 if __name__ == "__main__":
-    app = App(geometry="1600x900")
+    app = App(geometry="1450x900")
 
     # You would typically put this into the__init__ of your subclass of App:
     app.window.widget.title("Example application myTk")
 
-    label1 = Label("This is a label centered in grid position (0,0)")
+    label1 = Label("This is centered in grid position (0,0)")
     label1.grid_into(app.window, column=0, row=0, pady=5, padx=5, sticky="")
-    label2 = Label("This is another label top-aligned in grid position (0,1)")
+    label2 = Label("This is top-aligned in grid position (0,1)")
     label2.grid_into(app.window, column=1, row=0, pady=5, padx=5, sticky="n")
 
     box1 = Box(label="This is a labelled box in grid (0,2)")
     box1.grid_into(app.window, column=2, row=0, pady=5, padx=5, sticky="nsew")
     
-    thing1 = LabelledEntry(label="Some entry (centered)", character_width=5)
+    thing1 = LabelledEntry(label="Centerd entry", character_width=5)
     thing1.grid_into(box1, column=0, row=0, padx=10)
-    thing2 = LabelledEntry(label="Some other entry (left)", character_width=5)
+    thing2 = LabelledEntry(label="Left-aligned entry", character_width=5)
     thing2.grid_into(box1, column=0, row=1, padx=5, sticky='w')
 
     # app.window.widget.grid_columnconfigure(1, weight=1)
@@ -1348,13 +1348,13 @@ if __name__ == "__main__":
     for i in range(20):
         table.append(["Item {0}".format(i), "Something", "http://www.python.org"])
 
-    figure1 = Figure(figsize=(6, 4))
+    figure1 = Figure(figsize=(4, 3))
     figure1.grid_into(app.window, column=3, row=1, pady=5, padx=5)
     axis = figure1.figure.add_subplot()
     axis.plot([1, 2, 3], [4, 5, 6])
     axis.set_title("A matplotlib figure in grid position (3,1)")
 
-    some_fig = MPLFigure(figsize=(6, 4))
+    some_fig = MPLFigure(figsize=(4, 3))
     axis = some_fig.add_subplot()
     axis.plot([1, 2, 3], [-4, -5, -6])
     axis.set_title("You can provide your plt.figure")
@@ -1362,26 +1362,25 @@ if __name__ == "__main__":
     figure2 = Figure(figure=some_fig)
     figure2.grid_into(app.window, column=3, row=2, pady=5, padx=5)
 
-    try:
-        raise NotImplementedError()
-        video = VideoView(device=1)
-        video.zoom_level = 4
-        video.grid_into(app.window, column=0, row=2, pady=5, padx=5, sticky="")
-    except Exception as err:
-        video = Label("Unable to load VideoView")
-        video.grid_into(app.window, column=0, row=2, pady=5, padx=5, sticky="")
+    # try:
+    #     video = VideoView(device=0)
+    #     video.zoom_level = 5
+    #     video.grid_into(app.window, column=1, columnspan=2, row=2, pady=5, padx=5, sticky="")
+    # except Exception as err:
+    #     video = Label("Unable to load VideoView")
+    #     video.grid_into(app.window, column=1, row=2, pady=5, padx=5, sticky="")
 
     def i_was_changed(checkbox):
         showwarning(message="The checkbox was modified")
 
     view3 = View(width=100, height=100)
-    view3.grid_into(app.window, column=1, row=2, pady=5, padx=5, sticky="nsew")
+    view3.grid_into(app.window, column=0, row=2, pady=5, padx=5, sticky="nsew")
     view3.widget.grid_rowconfigure(0,weight=0)
     view3.widget.grid_rowconfigure(1,weight=0)
     view3.widget.grid_rowconfigure(2,weight=0)
     checkbox = Checkbox(label="Check me!", user_callback=i_was_changed)
     checkbox.grid_into(view3, column=0, row=0, pady=5, padx=5, sticky="nsew")
-    slider = Slider()
+    slider = Slider(width=50)
     slider.grid_into(view3, column=0, row=1, pady=5, padx=5, sticky="nsew")
     slider.value_variable.set(0)
     indicator = DoubleIndicator(value_variable=DoubleVar(value=0), format_string="Formatted slider value: {0:.1f}%")
@@ -1395,7 +1394,7 @@ if __name__ == "__main__":
     # view3.grid_into(app.window, column=0, row=0, pady=5, padx=5, sticky="nsew")
 
     canvas = CanvasView()
-    canvas.grid_into(app.window, column=2, row=2, pady=5, padx=5, sticky="nsew")
+    canvas.grid_into(app.window, column=1, row=2, pady=5, padx=5, sticky="nsew")
     canvas.widget.create_rectangle(
             4, 4, 200, 200, outline="black", fill="white", width=2
         )
