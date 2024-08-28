43ea6d744fdc0f9094ced338867bbe17c630aaeb
Daniel Côté <dccote@cervo.ulaval.ca>
2024-08-28 14:47:49 -0400

    Figured out how to use canvas
    Canvas is infact very powerful and handles all rendering of shapes, maiaintining the identities of the shapes..! This isn't

--- apps/canvas_app.py
+++ apps/canvas_app.py
@@ -2,14 +2,19 @@ import envapp
 from tkinter import DoubleVar
 from tkinter import filedialog
 from mytk import *
-from mytk.canvasview import CanvasView, CanvasElement, Rectangle, Oval, Line,BezierCurve
+from mytk.canvasview import CanvasView, CanvasElement, Rectangle, Oval, Line, Arrow, Vector, Axis, Axes, Label
 import time
 
-gLine = None
+def move():
+    element = canvas.element(axes.id)
+    element.move_by(1,1)
+    canvas.after(delay=100, function=move)
 
-def rotate_line():
-    gLine.angle += 10
-    canvas.draw()
+
+def move_by_coords():
+    element = canvas.element(oval.id)
+    element.coords = (100,100,200,200)
+    canvas.after(delay=10, function=move)
 
 if __name__ == "__main__":
     app = App()
@@ -18,22 +23,31 @@ if __name__ == "__main__":
     canvas = CanvasView(width=1000, height=900, background='white')
     canvas.grid_into(app.window, column=0, row=0, pady=5, padx=5, sticky="nsew")
 
-    rect = Rectangle(width=100, height=200, fill='lightblue', outline='black', outline_width=5)
+    rect = Rectangle(size=(100,200), fill='lightblue', outline='black', width=5)
     canvas.place(rect, position=(10,10))
-    oval = Oval(width=40, height=60, fill='green', outline='black', outline_width=2)
+    oval = Oval(size=(40, 60), fill='green', outline='black', width=2)
     canvas.place(oval, position=(200,100))
-    line = Line(length=100, angle=45, width=2, arrow='both')
-    canvas.place(line, position=(400,400))
     path = Line(points=((0,0),(100,100),(200,30),(300,100),(400,320)), width=2)
-    canvas.place(path, position=(300,100))
-    curve = BezierCurve(points=((0,0),(100,100), (150,100)), width=2)
-    canvas.place(curve, position=(400,400))
-    canvas.draw()
+    canvas.place(path, position=(300,300))
 
-    print(canvas.widget.itemconfigure(tagorid='123'))
-    # canvas.after(delay=1000, function=rotate_line)
+    arrow = Arrow(start=(50,30), end=(405,405), width=2)
+    canvas.place(arrow)
 
-    app.window.all_resize_weight(1)
+    path2 = Line(points=((0,0),(100,100),(200,30),(300,100),(400,320)), width=2)
+    canvas.place(path2)
+
+    axes = Axes( u=Vector(300,0), v=Vector(0,-100), width=2)
+    canvas.place(axes, position=(600,600))
+
+    axis = Axis( u=Vector(300,0), width=2)
+    canvas.place(axis, position=(600,600))
+
+    label = Label( text="Bonjour", font_size=20)
+    canvas.place(label, position=(600,600))
+
+    canvas.element(id)
+    canvas.after(delay=10, function=move)
+    canvas.after(delay=1000, function=move_by_coords)
     
     app.mainloop()
 
--- mytk/canvasview.py
+++ mytk/canvasview.py
@@ -1,108 +1,215 @@
 from tkinter import Canvas
-from math import cos, sin
+from tkinter import font
+from math import cos, sin, sqrt
 from .base import Base
 
+class Vector(tuple):
+    def __new__(cls, *args):
+        if len(args) == 2:
+            return tuple.__new__(cls, args)
+        else:
+            return tuple.__new__(cls, tuple(*args))
+
+    def __add__(self, rhs):
+        return Vector(self[0] + rhs[0], self[1] + rhs[1])
+
+    def __sub__(self, rhs):
+        return Vector(self[0] - rhs[0], self[1] - rhs[1])
+
+    def __mul__(self, scalar):
+        return Vector(self[0]*scalar, self[1]*scalar)
+
+    def length(self):
+        return sqrt(self[0]*self[0] + self[1]*self[1])
+
+class CoordinateSystem:
+    def __init__(self, origin, u, v):
+        self.origin = origin
+        self.u = u
+        self.v = v
 
 class CanvasView(Base):
     def __init__(self, width=200, height=200, **kwargs):
         super().__init__()
+        self.flip_coordinates = False
         self._widget_args = {"width": width, "height": height}
         self._widget_args.update(kwargs)
+        self.elements = []
+        self.coords_systems = {}
 
     def create_widget(self, master, **kwargs):
         self.widget = Canvas(master=master, **self._widget_args)
 
-    def place(self, element, position):
-        element.position = position
-        element.canvas = self
-        self.elements.append(element)
+    def place(self, element, position = None):
+        id = element.create(canvas=self, position=position)
+        # For reference: we want to work with our objects, not just the Tkinter id
+        self.elements.append(element) 
+        return id
 
-    def draw(self):
+    def element(self, id):
         for element in self.elements:
-            element.draw()
+            if element.id == id:
+                return element
+        return None
+
+    def create_coord_system(self, name, space_coords:(Vector,Vector), canvas_coords:(Vector,Vector)):
+        self.coords_systems[name] = (space_coords, canvas_coords)
+
+    def convert_to_coord_system(self, name, coords):
+        (space_start, space_end), (canvas_start, canvas_end) = self.coords_systems[name]
+        
+
+    # def convert_from_coord_system(self, name, space_coords):
 
 class CanvasElement:
     def __init__(self, **kwargs):
         self.id = None
-        self._element_args = kwargs
-        if "outline_width" in self._element_args:
-            self._element_args['width'] = self._element_args['outline_width']
-            self._element_args.pop('outline_width')
-        self.position = None
-        self.canvas = None
-        self.anchor = None
-    
-    def place(self, canvas, position, anchor='w'):
-        self.canvas = canvas
-        self.position = (x,y)
-        self.anchor = anchor
+        self._element_kwargs = kwargs
 
-    def draw(self):
-        pass
+    def itemconfigure(self, **kwargs):
+        return self.canvas.itemconfigure(self.id, **kwargs)
+
+    @property
+    def coords(self):
+        return self.canvas.widget.coords(self.id)
+
+    @coords.setter
+    def coords(self, new_coords):
+        return self.canvas.widget.coords(self.id, new_coords)
+
+    @property
+    def tags(self):
+        return self.canvas.widget.gettags(self.id)
+
+    def add_tag(self, tag):
+        return self.canvas.widget.addtag_withtag(tag, self.id)        
+
+    def move_by(self, dx, dy):
+        self.canvas.widget.move(self.id, dx, dy)
+
+    def create(self, canvas, position):
+        pass        
+
 
 class Rectangle(CanvasElement):
-    def __init__(self, width, height, **kwargs):
-        CanvasElement.__init__(self, **kwargs)
-        self.width = width
-        self.height = height
+    def __init__(self, size:(int, int), **kwargs):
+        super().__init__(**kwargs)
+        self.size = Vector(size)
 
-    def create(self):
-        top_left_x = self.position[0]
-        top_left_y = self.position[1]
-        bottom_right_x = top_left_x + self.width
-        bottom_right_y = top_left_y + self.height
+    def create(self, canvas, position):
+        self.canvas = canvas
+        top_left = Vector(position)
+        bottom_right = top_left + self.size
 
-        self.id = self.canvas.widget.create_rectangle(
-                (top_left_x, top_left_y, bottom_right_x, bottom_right_y), **self._element_args
+        self.id = canvas.widget.create_rectangle(
+                (top_left, bottom_right), **self._element_kwargs
             )
+        return self.id
 
 class Oval(CanvasElement):
-    def __init__(self, width, height, **kwargs):
-        CanvasElement.__init__(self, **kwargs)
-        self.width = width
-        self.height = height
+    def __init__(self, size, **kwargs):
+        super().__init__(**kwargs)
+        self.size = Vector(size)
 
-    def place(self, position):
-        top_left_x = self.position[0]
-        top_left_y = self.position[1]
-        bottom_right_x = top_left_x + self.width
-        bottom_right_y = top_left_y + self.height
+    def create(self, canvas, position):
+        self.canvas = canvas
+        top_left = Vector(position)-self.size*0.5
+        bottom_right = top_left + self.size
 
-        self.id = self.canvas.widget.create_oval(
-                (top_left_x, top_left_y, bottom_right_x, bottom_right_y), **self._element_args
+        self.id = canvas.widget.create_oval(
+                (top_left, bottom_right), **self._element_kwargs
             )
+
+        return self.id
 
 class Line(CanvasElement):
-    def __init__(self, points=None, length=None, angle=None, **kwargs):
-        CanvasElement.__init__(self, **kwargs)
-        self.length = None
-        self.angle = None
-        self.points = None
-
-        if points is not None:
-            self.points = points
-        else:
-            self.length = length
-            self.angle = angle
-
-    def draw(self):
-        canvas_points = []
-        if self.points is not None:
-            for point in self.points:
-                canvas_points.append( (self.position[0] + point[0], self.position[1] + point[1]))
-        else:
-            start_x = self.position[0]
-            start_y = self.position[1]
-            end_x = start_x + self.length*cos(-self.angle * 3.1416/180)
-            end_y = start_y + self.length*sin(-self.angle * 3.1416/180)
-            canvas_points.append( (start_x, start_y, end_x, end_y))
-
-        self.canvas.widget.create_line(
-                canvas_points, **self._element_args, tag='123',  smooth=False
-            )
-
-class BezierCurve(CanvasElement):
-    def __init__(self, points, **kwargs):
-        CanvasElement.__init__(self, **kwargs)
+    def __init__(self, points=None, **kwargs):
+        super().__init__(**kwargs)
         self.points = points
 
+    def create(self, canvas, position = None):
+        self.canvas = canvas
+
+        canvas_points = []
+        for point in self.points:
+            if position is not None:
+                canvas_points.append( Vector(position) + Vector(point) )
+            else:
+                canvas_points.append( point )
+
+        self.id = canvas.widget.create_line(
+                canvas_points, smooth=False, **self._element_kwargs,  
+            )
+        return self.id
+
+
+class Arrow(Line):
+    def __init__(self, start, end=None, length=None, angle=None, **kwargs):
+        kwargs['arrow'] = 'last'
+        if 'width' not in kwargs:
+            kwargs['width'] = 2
+        super().__init__(points=(start, end), **kwargs)
+
+class Axis(CanvasElement):
+    def __init__(self, u, **kwargs):
+        super().__init__(**kwargs)
+
+        self.u = Vector(u)
+        self.axis = None
+        self.origin = None
+
+    def create(self, canvas, position = None):
+        self.canvas = canvas
+        self.id = "my_axis"
+
+        width = self._element_kwargs.get('width',1)
+
+        self.origin = Oval(size=(1.5*width,1.5*width), fill='black', tag=(self.id), **self._element_kwargs)
+        self.origin.create(canvas, position)
+        self.axis = Arrow(start=Vector(0,0), end=self.u, tag=(self.id,), **self._element_kwargs)
+        self.axis.create(canvas, position)
+
+        return self.id
+
+class Axes(CanvasElement):
+    def __init__(self, u, v, **kwargs):
+        super().__init__(**kwargs)
+
+        self.u = Vector(u)
+        self.v = Vector(v)
+        self.x_axis = None
+        self.y_axis = None
+        self.origin = None
+
+    def create(self, canvas, position = None):
+        self.canvas = canvas
+        self.id = "my_axes"
+
+        width = self._element_kwargs.get('width',1)
+
+        self.origin = Oval(size=(1.5*width,1.5*width), fill='black', tag=(self.id), **self._element_kwargs)
+        self.origin.create(canvas, position)
+        self.x_axis = Arrow(start=Vector(0,0), end=self.u, tag=(self.id,), **self._element_kwargs)
+        self.x_axis.create(canvas, position)
+        self.y_axis = Arrow(start=Vector(0,0), end=self.v, tag=(self.id,), **self._element_kwargs)
+        self.y_axis.create(canvas, position)
+
+        return self.id
+
+
+class Label(CanvasElement):
+    def __init__(self, font_size=20, **kwargs):
+        super().__init__(**kwargs)
+        self.font_size = font_size
+
+    def create(self, canvas, position = None):
+        self.canvas = canvas
+        if position is None:
+            position = Vector(0,0)
+
+        f = font.nametofont('TkTextFont')
+        f['size'] = self.font_size
+        self.id = canvas.widget.create_text(
+                position, **self._element_kwargs,  font=f
+            )
+        return self.id
