fd12cd849b6ae09497b0ddadaf3f6ea6e8dc4bee
Daniel Côté <dccote@cervo.ulaval.ca>
2025-01-27 12:21:15 -0500

    Clean up of powermeter_app.py

--- mytk/example_apps/powermeter_app.py
+++ mytk/example_apps/powermeter_app.py
@@ -33,6 +33,7 @@ class PowerMeterApp(App):
 
         self.box = Box("Actions")
         self.box.grid_into(self.window, row=0, column=3, padx=25, pady=25, sticky="nsew")
+
         self.start_button = Button("Start", user_event_callback=self.click_start)
         self.start_button.grid_into(self.box, row=0, column=0, padx=10, pady=10, sticky="ns")
         self.save_button = Button("Save data…", user_event_callback=self.click_save)
@@ -43,25 +44,30 @@ class PowerMeterApp(App):
         self.wavelength_entry.grid_into(self.box, row=3, column=0)
 
         self.device = PowerMeterDevice()
+        # self.device.bind_properties("wavelength", self.wavelength_entry.entry, "value_variable")
+        self.is_refreshing = False
 
-        self.update_device_power()
-
-    def update_device_power(self):
-        self.device.update_power()
-
-        self.measurement_label.value_variable.set(f"{self.device.power:.1f} W")
-        
-        last = len(self.plot.x)
-        self.plot.append(last, self.device.power)
-        self.plot.update_plot()
-
-        self.after(500, self.update_device_power)
 
     def click_start(self, event, button):
-        if button.label == "Start":
+        if not self.is_refreshing:
+            self.is_refreshing = True
+            self.update_device_power()
             button.label = "Stop"
         else:
+            self.is_refreshing = False
             button.label = "Start"
+
+    def update_device_power(self):
+        power = self.device.get_power()
+
+        self.measurement_label.value_variable.set(f"{power:.1f} W")
+        
+        last = len(self.plot.x)
+        self.plot.append(last, power)
+        self.plot.update_plot()
+
+        if self.is_refreshing:
+            self.after(500, self.update_device_power)
 
     def click_save(self, event, button):
         filepath = filedialog.asksaveasfilename(
@@ -73,6 +79,9 @@ class PowerMeterApp(App):
             pass # Do something
 
     def click_refresh(self, event, button):
+        wavelength = self.wavelength_entry.entry.value_variable.get()
+        self.device.wavelength = wavelength
+
         self.table.data_source.remove_all_records()
 
         device_info = self.device.device_info()
@@ -80,27 +89,37 @@ class PowerMeterApp(App):
             self.table.data_source.append_record({"parameter":parameter, "value":value})
 
 
-class PowerMeterDevice:
+class PowerMeterDevice(Bindable):
     debug = True
 
     def __init__(self):
-        self.power = 0
+        super().__init__()
 
-    def update_power(self):
+        self.power = 0
+        self.wavelength = 1064
+
+    def get_power(self):
         if self.debug:
             self.power = random.randrange(90,100,1)
         else:
-            pass # read from USB
+            pass # Update via USB
+
+        return self.power
 
     def something(self):
         pass
 
     def device_info(self):
-        return {"Board temperature":74.5,
+        info = {}
+        if self.debug:
+            info = {"Board temperature":74.5,
                 "Firmware version":"1.0.0",
-                "Wavelength [nm]":1064
+                "Wavelength [nm]": self.wavelength
                 }
+        else:
+            pass # Update via USB
 
+        return info
 
 if __name__ == "__main__":
     app = PowerMeterApp()
